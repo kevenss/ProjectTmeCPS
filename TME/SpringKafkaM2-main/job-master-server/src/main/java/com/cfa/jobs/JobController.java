a5e593f531ebeea280d98c9250ae470897b89381
kevenss <92720711+kevenss@users.noreply.github.com>
2022-05-27 22:15:11 +0200

    ajouter getbyId, save et getall sur jobcontroller

--- TME/SpringKafkaM2-main/job-master-server/src/main/java/com/cfa/jobs/JobController.java
+++ TME/SpringKafkaM2-main/job-master-server/src/main/java/com/cfa/jobs/JobController.java
@@ -1,5 +1,7 @@
 package com.cfa.jobs;
 
+import com.cfa.objects.letter.Letter;
+import com.cfa.objects.letter.LetterRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -7,9 +9,10 @@ import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Controller to launch jobs from an API call
@@ -19,7 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(produces = "application/json; charset=UTF-8", value = "/v1/jobcontroller")
 @RequiredArgsConstructor
 public class JobController {
-
+  @Autowired
+  private LetterRepository letterRepository;
   private final JobLauncher jobLauncher;
   private final Job simpleJob;
   private final Job messageLetterJob;
@@ -33,7 +37,18 @@ public class JobController {
   public void messageLetterJob(@RequestParam(value = "message") final String label) {
     runJobB(this.messageLetterJob, label);
   }
-
+  @GetMapping("/getletters")
+  public List<Letter> getAll(){
+    return letterRepository.findAll();
+  }
+  @PostMapping("/postLetter/l")
+  public Letter save(@RequestBody Letter l) {
+    return letterRepository.save(l);
+  }
+  @GetMapping("/getById/{id}")
+  public Letter getById(@PathVariable("id") int id){
+    return letterRepository.getOne(id);
+  }
   private void runJobB(final Job parJob, final String label) {
     final JobParameters locParamJobParameters = new JobParametersBuilder()
       .addParameter("value", new JobParameter(label))
